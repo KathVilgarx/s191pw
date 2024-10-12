66ab5c6786085a448e7bafaeb7bd9a0a8922b749
Katherine Andrea Villanueva Garcia <122043747@upq.edu.mx>
2024-10-11 20:22:31 -0600

    Practica No.4: Controladores

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+Route::get('/',[controladorVistas::class,'home'])->name('rutainicio');
+
+
+Route::get('/form',[controladorVistas::class,'formulario'])->name('rutacacas');
+
+
+Route::get('/consultar',[controladorVistas::class,'clientes'])->name('rutaconsulta');
 
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
-Route::view('/', 'inicio') ->name('rutainicio');
+/* Route::view('/', 'inicio') ->name('rutainicio');
 
 Route::view('/form','formulario') ->name('rutacacas');
 
-Route::view('/consultar','clientes') ->name('rutaconsulta');
+Route::view('/consultar','clientes') ->name('rutaconsulta'); */
 
 Route::view('/component','componentes') ->name('componentes');
 
