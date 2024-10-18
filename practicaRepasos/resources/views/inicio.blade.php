dc9fc7d6de1183c72db61d76aa87d4ad77505776
Katherine Andrea Villanueva Garcia <122043747@upq.edu.mx>
2024-10-18 14:50:44 -0600

    3. Portada

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Inicio</title>
+  @vite(['resources/js/app.js'])
+  <style>
+    body {
+      margin: 0;
+      height: 100vh;
+      display: grid;
+      place-items: center; /* Centra horizontal y verticalmente */
+    }
+
+    .card {
+      width: 300px;
+      padding: 20px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      border-radius: 10px;
+      background-color: white;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+<div class="container">
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="imagen random equisdededede">
+  <div class="card-body">
+    <h5 class="card-title">Repaso 1</h5>
+    <p class="card-text">Programacion Web</p>
+    <p class="card-text">Grupo S-191</p>
+    <p class="card-text">Katherine Andrea Villanueva Garcia</p>
+    <p class="card-text">Mtro. Ivan Isay Guerra Lopez</p>
+
+  </div>
+</div>
+
+
+
+<!---->
+</body>
+</html>
